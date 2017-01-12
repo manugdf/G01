4ac68791171ca9e3eec5816b0bdf4a4c29199aaa
manugdf <manugdf94@gmail.com>
2017-01-13 00:43:22 +0100

    Funcionalidad multiplicación y test

---
+++ EGC/src/multiplicacion/Multiplicación.java
@@ -0,0 +1,10 @@
+package multiplicacion;
+
+public class Multiplicación {
+
+	public static int multiplicacion(int a,int b){
+		
+		return a*b;
+	}
+	
+}
---
+++ EGC/src/multiplicacion/TestMultiplicacion.java
@@ -0,0 +1,14 @@
+package multiplicacion;
+
+public class TestMultiplicacion {
+
+public static void main (String args[]){
+		
+		if(Multiplicación.multiplicacion(1, 2)!=2){
+			System.out.println("No funciona");
+		}else{
+			System.out.println("Funciona");
+		}
+	}
+	
+}
